356c82f528546b4d0b5fa0f84b57e228fc047d8e
alper.yurtdas06@gmail.com <alper.yurtdas06@gmail.com>
2021-03-04 22:17:08 +0300

    Fehmi Arslan

---
+++ SliderandSearchBox/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DersCalisma.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Jquery() 
+        {
+            return View();
+        }
+        public ActionResult SearchBox()
+        {
+            return View();
+        }
+        public ActionResult Slider()
+        {
+            return View();
+        }
+    }
+}
---
+++ SliderandSearchBox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DersCalisma.MvcApplication" Language="C#" %>
